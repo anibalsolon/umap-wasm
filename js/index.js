640822af6399b25fc225f4be360937b08d6890b6
Anibal Solon <anibalsolon@gmail.com>
2021-03-13 17:50:00 -0500

    feat: inverse matrix impl

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-import("../pkg/index.js").catch(console.error);
+import("../pkg/index.js")
+  .then((wasm) => {
+    wasm.greet("WebAssembly");
+
+    let buffer = new wasm.WasmMatrix(2, 2, array => {
+      array[0] = 4;
+      array[1] = 7;
+      array[2] = 2;
+      array[3] = 6;
+    });
+    console.log(buffer.get());
+
+    let inv = buffer.inv();
+    console.log(inv.get());
+
+    buffer.free();
+    inv.free();
+  })
+  .catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
+
+extern crate nalgebra as na;
+extern crate js_sys;
+
 use wasm_bindgen::prelude::*;
 use web_sys::console;
+use js_sys::Float32Array;
+use js_sys::Function;
+use na::{DMatrix};
 
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
@@ -14,14 +21,61 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
-
-
-    // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
-
     Ok(())
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    console::log_1(&JsValue::from_str(&format!("Hello, {}!", name)));
+}
+
+#[wasm_bindgen]
+pub struct WasmMatrix {
+    rows: usize,
+    cols: usize,
+    buffer: Vec<f32>,
+}
+
+#[wasm_bindgen]
+impl WasmMatrix {
+    #[wasm_bindgen(constructor)]
+    pub fn new(rows: i32, cols: i32, f: &Function) -> Self {
+        let mut buffer = vec![0f32; (rows * cols) as usize];
+        unsafe {
+            let array = Float32Array::view(&mut buffer);
+            f.call1(&JsValue::NULL, &JsValue::from(array))
+                .expect("The callback function should not throw");
+        }
+        Self { buffer, rows: rows as usize, cols: cols as usize }
+    }
+
+    #[wasm_bindgen]
+    pub fn inv(&self) -> Result<WasmMatrix, JsValue> {
+        let m = DMatrix::from_row_slice(self.rows, self.cols, &self.buffer);
+        match m.try_inverse() {
+            Some(inv) => {
+                unsafe {
+                    Ok(Self {
+                        // TODO review so many reallocations
+                        buffer: inv.transpose().as_slice().to_vec(),
+                        rows: inv.nrows(),
+                        cols: inv.ncols()
+                    })
+                }
+            }
+            None => {
+                Err(JsValue::from_str("Matrix is not inversible"))
+            }
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn get(&self) -> Float32Array {
+        unsafe {
+            Float32Array::view(self.buffer.as_slice())
+        }
+    }
 }
